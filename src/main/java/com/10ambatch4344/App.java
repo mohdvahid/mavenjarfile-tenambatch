606e554a575b287446a60e9c5a4b9a09758cdc50
mohdvahid <mohdvahidpasha85@gmail.com>
2023-04-25 19:19:59 +0530

    hello

---
+++ src/main/java/com/10ambatch4344/App.java
@@ -0,0 +1,13 @@
+package com.10ambatch4344;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
